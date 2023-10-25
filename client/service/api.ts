bba3d92acace30b4313d3b1034acf2aad33b5f7d
Emmanueltoluwanimi <emmanueltoluwanimi01@gmail.com>
2023-10-26 00:09:37 +0100

    fix: bug

--- client/service/api.ts
+++ client/service/api.ts
@@ -1,4 +1,12 @@
-const baseUrl = process.env.NODE_ENV === 'production' ? "https://www.kisi-api.com/api/images" : `http://localhost:6600/api/images`
+const baseUrl = process.env.NODE_ENV === 'production' ? "https://www.kisi-api.com/api/images" : `http://localhost:6600/api/images`;
+
+interface IServiceResponse {
+  status?: string;
+  statusCode?: number;
+  message?: string;
+  data:any
+}
+
 
 export async function GetImagesApi():Promise<IServiceResponse> {
   try {
